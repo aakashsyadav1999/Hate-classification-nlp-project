baf0453aa2bd0a4a2f0f1fa6692decdd897c7c6c
Aakash Yadav <aakashsyadav1999@gmail.com>
2024-02-21 11:47:11 +0530

    just created

---
+++ nlp_end_end_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project include end to end nlp hate detection project',
+    author='aakash',
+    license='',
+)
